dee8b93e8850a0e60e54a3ef9ecbfd1a78a7fa8a
Scott Feeney <graue@oceanbase.org>
2009-02-13 13:54:20 -0500

    Expression evaluation and definitions.
    darcs-hash:20090213185420-bbafa-d595606f4f6aaad23bc5791fd5c8160c10c5a799

---
+++ expr.c
@@ -0,0 +1,49 @@
+#include <math.h>
+#include <assert.h>
+#include "expr.h"
+
+// primitives that are not already functions
+float add(float a, float b) { return a + b; }
+float sub(float a, float b) { return a - b; }
+float mul(float a, float b) { return a * b; }
+float div(float a, float b) { return b == 0.0 ? 0.0 : a / b; }
+float mod(float a, float b) { return b == 0.0 ? 0.0 : fmodf(a, b); }
+
+const operator_t opers[] =
+{
+	{ "+", 2, { .func2 = add }},
+	{ "-", 2, { .func2 = sub }},
+	{ "*", 2, { .func2 = mul }},
+	{ "/", 2, { .func2 = div }},
+	{ "%", 2, { .func2 = mod }},
+	{ "sin", 1, { .func1 = sinf }},
+	{ "cos", 1, { .func1 = cosf }}
+};
+
+const int numopers = sizeof opers / sizeof opers[0];
+
+float evaluate(expr_t *expr, float x, float y, float t)
+{
+	float f1, f2;
+	const operator_t *oper;
+
+	if (expr->type == EXPR_CONSTANT)
+		return expr->details.num;
+	if (expr->type == EXPR_X)
+		return x;
+	if (expr->type == EXPR_Y)
+		return y;
+	if (expr->type == EXPR_T)
+		return t;
+
+	assert(expr->type == EXPR_OP);
+
+	f1 = evaluate(expr->details.op.args[0], x, y, t);
+
+	oper = expr->details.op.oper;
+	if (oper->nargs == 1)
+		return oper->func.func1(f1);
+
+	f2 = evaluate(expr->details.op.args[1], x, y, t);
+	return oper->func.func2(f1, f2);
+}
---
+++ expr.h
@@ -0,0 +1,35 @@
+typedef float (*op1_t)(float);
+typedef float (*op2_t)(float, float);
+
+typedef struct
+{
+	const char *name;
+	int nargs; // 1 or 2
+	union {
+		op1_t func1;
+		op2_t func2;
+	} func;
+} operator_t;
+
+extern const operator_t opers[];
+extern const int numopers;
+
+// an expression can be:
+//   a constant number
+//   a variable (x, y, or t)
+//   an operator with 1 or 2 expression(s) as argument(s)
+enum { EXPR_CONSTANT, EXPR_X, EXPR_Y, EXPR_T, EXPR_OP };
+
+typedef struct expr_s
+{
+	int type;
+	union {
+		float num; // EXPR_CONSTANT
+		struct {
+			const operator_t *oper;
+			struct expr_s *args[2];
+		} op; // EXPR_OP
+	} details;
+} expr_t;
+
+float evaluate(expr_t *expr, float x, float y, float t);
